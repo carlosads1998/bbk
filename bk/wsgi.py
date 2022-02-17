4211218406c314643c88e3eb3eacc8dd369b5f5d
carlosads1998 <carlos.ads1998@gmail.com>
2022-02-17 17:22:19 -0300

    Alt

--- bk/wsgi.py
+++ bk/wsgi.py
@@ -8,11 +8,11 @@ https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
 """
 
 import os
-from dj_static import Cling
+
 from django.core.wsgi import get_wsgi_application
 
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bk.settings')
 
 
-application = Cling(get_wsgi_application())
+application = get_wsgi_application
